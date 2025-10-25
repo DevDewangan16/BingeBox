80934b5b0bc7193c730a61807fca0d68f98e6bd2
Dev Dewangan <138326598+DevDewangan16@users.noreply.github.com>
2025-10-25 17:16:55 +0530

    Update API_KEY to placeholder value
    Replaced the API key with a placeholder for security.

--- app/src/main/java/com/example/movieapp/util/Constants.kt
+++ app/src/main/java/com/example/movieapp/util/Constants.kt
@@ -2,5 +2,5 @@ package com.example.movieapp.util
 
 object Constants {
     const val BASE_URL = "https://api.watchmode.com/v1/"
-    const val API_KEY = "Evjg5VrfIUSNootxruUa5GyXv6dzB9R14dvSkZ1b"
+    const val API_KEY = "Add_your_api_key"
 }
